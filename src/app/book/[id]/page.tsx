38515cabc04ae756bf0ac4385245ae38dbc9dd62
Uziel Valdez <uzielvaldez85@gmail.com>
2024-05-23 18:42:22 -0700

    feat: improve dynamic routest (#7)

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -6,26 +6,39 @@ import Image from "next/image";
 export default async function Book({ params }: {
   params: { id: string },
 }) {
-  // doing it this way since I didn't add state management, next js memoize and cache fetch
-  const books = await getBooks();
-  const book = books.filter((i:IBook) => i.id === params.id)[0];
-  if (!book) {
+  const bookData = await getPostData(params)
+  if (!bookData) {
     return <h1>Book not found!</h1>;
   }
 
   return (
     <div className="max-w-full items-center justify-between text-center">
       <h1 className="font-bold mb-5 max-w-full">
-        {book.title}
+        {bookData.title}
       </h1>
       <section className="shadow-2xl p-5 mb-10 mx-auto">
-        <Image src={book.imageUrl} width={125} height={200} alt='test' className="m-4" style={{ margin: 'auto' }}/>
+        <Image src={bookData.imageUrl} width={125} height={200} alt='test' className="m-4" style={{ margin: 'auto' }}/>
         <div>
-          <span className='font-medium text-xs mb-3'>{book.author}</span>
-          <p className='text-xs mb-10'>{book.description}</p>
+          <span className='font-medium text-xs mb-3'>{bookData.author}</span>
+          <p className='text-xs mb-10'>{bookData.description}</p>
         </div>
       </section>
       <Link href="/" className="text-white bg-green font-medium rounded-lg text-center text-sm h-11 px-5 py-2.5 me-2 mb-2">Go Home</Link>
     </div>
   )
 }
+
+
+export async function getPostData(params: {id: string}) {
+  const books = await getBooks();
+  const bookData = books.filter((book: IBook) => book.id === params.id)[0];
+  return bookData
+}
+
+export async function generateStaticParams() {
+  const books = await getBooks();
+  return books.map((book: IBook) => {
+    return { id: book.id }
+  });
+}
+
